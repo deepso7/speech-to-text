9ffa0b6c0b1c76258cb3dca6decf2fb09b305fdf
deepso <deeps.o7o8.y@gmail.com>
2024-08-31 01:40:39 +0530

    setup dark mode

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,10 @@ import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <>
+    <div className="vertical center pt-8 space-y-4">
       <h1 className="text-3xl font-bold">Hello world!</h1>
       <Button>Click me</Button>
-    </>
+    </div>
   );
 }
 
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,10 +2,13 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@/components/theme-provider";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
